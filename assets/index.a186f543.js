0d7b5cd8dbe2b92df4dff02c700afb4baf0ee7b8
0b5vr <0b5vr@users.noreply.github.com>
2023-11-08 15:21:03 +0000

    Automated deployment: Wed Nov  8 15:21:03 UTC 2023 531a845a582e7b92a63690ee3c4e186342bd1e02

--- assets/index.a186f543.js
+++ assets/index.a186f543.js
@@ -734,33 +734,55 @@ uniform sampler2D samplerRandom;
 
 out vec4 fragColor;
 
+const int SAMPLES_PER_FRAME = 8;
+const int REFLECTIONS_PER_SAMPLE = 4;
+
 const float PI = acos( -1. );
 const float TAU = PI * 2.0;
 const float EPSILON = 1E-4;
 const float FAR = 1E3;
 
-vec4 seed;
-
-// Ref: https://cs.uwaterloo.ca/~thachisu/tdf2015.pdf
-float random() {
-  const vec4 q = vec4( 1225, 1585, 2457, 2098 );
-  const vec4 r = vec4( 1112, 367, 92, 265 );
-  const vec4 a = vec4( 3423, 2646, 1707, 1999 );
-  const vec4 m = vec4( 4194287, 4194277, 4194191, 4194167 );
-    vec4 beta = floor( seed / q );
-    vec4 p = a * ( seed - beta * q ) - beta * r;
-    beta = ( sign( -p ) + vec4( 1 ) ) * vec4( 0.5 ) * m;
-    seed = ( p + beta );
-    return fract( dot( seed / m, vec4( 1, -1, 1, -1 ) ) );
+float safeDot( vec3 a, vec3 b ) {
+  return max( dot( a, b ), 1E-6 );
 }
 
-vec2 random2() {
-  return vec2( random(), random() );
+// http://www.jcgt.org/published/0009/03/02/
+uvec3 hash3u( uvec3 v ) {
+  v = v * 1664525u + 1013904223u;
+
+  v.x += v.y * v.z;
+  v.y += v.z * v.x;
+  v.z += v.x * v.y;
+
+  v ^= v >> 16u;
+
+  v.x += v.y * v.z;
+  v.y += v.z * v.x;
+  v.z += v.x * v.y;
+
+  return v;
+}
+
+vec3 hash3f( vec3 v ) {
+  uvec3 r = hash3u( floatBitsToUint( v ) );
+  return vec3( r ) / float( -1u );
+}
+
+uvec3 seed;
+uvec3 random3u() {
+  seed = hash3u( seed );
+  return seed;
+}
+
+vec3 random3f() {
+  uvec3 r = random3u();
+  return vec3( r ) / float( -1u );
 }
 
 vec3 randomSphere() {
-  float phi = TAU * random();
-  float theta = acos( 1.0 - 2.0 * random() );
+  vec3 xi = random3f();
+  float phi = TAU * xi.x;
+  float theta = acos( 1.0 - 2.0 * xi.y );
   return vec3(
     cos( phi ) * sin( theta ),
     sin( phi ) * sin( theta ),
@@ -787,10 +809,15 @@ vec3 F_Schlick( vec3 f0, float VdotH ) {
   return mix( f0, vec3( 1.0 ), ( v * v ) * ( v * v ) * v );
 }
 
+float F_Schlick( float f0, float VdotH ) {
+  float v = 1.0 - VdotH;
+  return mix( f0, 1.0, ( v * v ) * ( v * v ) * v );
+}
+
 float G_Smith( float roughness, float NdotV, float NdotL ) {
-  float k = roughness;
-  k = k * k;
-  k /= 2.0;
+  float a = roughness * roughness;
+  float a2 = a * a;
+  float k = a2 / 2.0;
 
   float G1V = NdotV / ( NdotV * ( 1.0 - k ) + k );
   float G1L = NdotL / ( NdotL * ( 1.0 - k ) + k );
@@ -799,8 +826,9 @@ float G_Smith( float roughness, float NdotV, float NdotL ) {
 }
 
 vec3 importanceSampleLambert( vec3 N ) {
-  float phi = TAU * random();
-  float cosTheta = random();
+  vec3 xi = random3f();
+  float phi = TAU * xi.x;
+  float cosTheta = xi.y;
   cosTheta = cos( asin( sqrt( cosTheta ) ) );
   float sinTheta = sqrt( 1.0 - cosTheta * cosTheta );
 
@@ -812,9 +840,13 @@ vec3 importanceSampleLambert( vec3 N ) {
 }
 
 vec3 importanceSampleGGX( float roughness, vec3 N ) {
-  float phi = TAU * random();
-  float cosTheta = random();
-  cosTheta = sqrt( ( 1.0 - cosTheta ) / ( 1.0 + ( pow( roughness, 4.0 ) - 1.0 ) * cosTheta ) );
+  float a = roughness * roughness;
+  float a2 = a * a;
+
+  vec3 xi = random3f();
+  float phi = TAU * xi.x;
+  float cosTheta = xi.y;
+  cosTheta = sqrt( ( 1.0 - cosTheta ) / ( 1.0 + ( a2 - 1.0 ) * cosTheta ) );
   float sinTheta = sqrt( 1.0 - cosTheta * cosTheta );
 
   return orthBas( N ) * vec3(
@@ -889,17 +921,17 @@ Isect map( vec3 ro, vec3 rd ) {
   mtl = Material( vec3( 0.8, 0.2, 0.1 ), 0.1, 0.0, vec3( 0.0 ) );
   Isect isect = isectSphere( ro - vec3( -2.5, 0.0, 0.0 ), rd, 1.0, mtl );
 
-  mtl = Material( vec3( 0.8, 0.2, 0.1 ), 0.5, 0.0, vec3( 0.0 ) );
+  mtl = Material( vec3( 0.0 ), 0.5, 0.0, vec3( 0.0 ) );
   isect = isectMin( isect, isectSphere( ro, rd, 1.0, mtl ) );
 
-  mtl = Material( vec3( 0.8, 0.2, 0.1 ), 0.9, 0.0, vec3( 0.0 ) );
+  mtl = Material( vec3( 0.3, 0.4, 0.5 ), 0.9, 0.0, vec3( 0.0 ) );
   isect = isectMin( isect, isectSphere( ro - vec3( 2.5, 0.0, 0.0 ), rd, 1.0, mtl ) );
 
   mtl = Material( vec3( 0.8 ), 0.1, 0.0, vec3( 0.0 ) );
   isect = isectMin( isect, isectPlane( ro + vec3( 0.0, 1.0, 0.0 ), rd, vec3( 0.0, 1.0, 0.0 ), mtl ) );
 
-  mtl = Material( vec3( 0.0 ), 0.0, 0.0, vec3( 10.0, 20.0, 30.0 ) );
-  isect = isectMin( isect, isectSphere( ro - vec3( 10.0, 10.0, 10.0 ), rd, 5.0, mtl ) );
+  mtl = Material( vec3( 0.0 ), 0.0, 0.0, vec3( 50.0 / PI ) );
+  isect = isectMin( isect, isectSphere( ro - vec3( 0.0, 20.0, 0.0 ), rd, 5.0, mtl ) );
 
   return isect;
 }
@@ -907,69 +939,73 @@ Isect map( vec3 ro, vec3 rd ) {
 void main() {
   vec2 uv = gl_FragCoord.xy / resolution;
 
-  seed = texture( samplerRandom, uv );
-  seed += float( frame );
-  random();
+  seed = uvec3( gl_FragCoord.xy, frame );
 
-  vec2 p = ( gl_FragCoord.xy + random2() - 0.5 ) / resolution.xy - 0.5;
-  p.x *= resolution.x / resolution.y;
+  for ( int iSample = 0; iSample < SAMPLES_PER_FRAME; iSample ++ ) {
+    vec2 p = ( gl_FragCoord.xy + random3f().xy - 0.5 ) / resolution.xy - 0.5;
+    p.x *= resolution.x / resolution.y;
 
-  vec3 ro = vec3( 0, 0, 5 );
-  vec3 rd = normalize( vec3( p, -1 ) );
+    vec3 ro = vec3( 0, 0, 5 );
+    vec3 rd = normalize( vec3( p, -1 ) );
 
-  vec3 col = vec3( 0 );
-  vec3 colRem = vec3( 1 );
+    vec3 col = vec3( 0 );
+    vec3 colRem = vec3( 1 );
 
-  for ( int iRef = 0; iRef < 6; iRef ++ ) {
-    Isect isect = map( ro, rd );
+    for ( int iRef = 0; iRef < REFLECTIONS_PER_SAMPLE; iRef ++ ) {
+      Isect isect = map( ro, rd );
 
-    if ( isect.l >= FAR ) {
-      // col += 0.5 * colRem;
-      break;
+      if ( isect.l >= FAR ) {
+        // col += 0.5 * colRem;
+        break;
+      }
+      col += isect.mtl.emissive * colRem;
+
+      ro += rd * isect.l + isect.N * EPSILON;
+
+      vec3 albedo = mix( 0.96 * isect.mtl.albedo, vec3( 0.0 ), isect.mtl.metallic );
+      vec3 f0 = mix( vec3( 0.04 ), isect.mtl.albedo, isect.mtl.metallic );
+
+      float NdotV = safeDot( isect.N, -rd );
+      float Fn = mix(
+        F_Schlick( 0.04, NdotV ),
+        1.0,
+        isect.mtl.metallic
+      );
+
+      if ( random3f().x < Fn ) {
+        // specular
+        // Ref: http://gregory-igehy.hatenadiary.com/entry/2015/02/26/154142
+        vec3 H = importanceSampleGGX( isect.mtl.roughness, isect.N );
+        vec3 wo = reflect( rd, H );
+        if ( dot( wo, isect.N ) < 0.0 ) { break; }
+
+        float VdotH = safeDot( -rd, H );
+        float NdotL = safeDot( isect.N, wo );
+        float NdotH = safeDot( isect.N, H );
+        vec3 F = F_Schlick( f0, VdotH );
+        float G = G_Smith( isect.mtl.roughness, NdotV, NdotL );
+
+        colRem *= F * G * VdotH / ( NdotH * NdotV ) / Fn;
+        rd = wo;
+      } else {
+        // diffuse
+        vec3 wo = importanceSampleLambert( isect.N );
+        vec3 H = normalize( -rd + wo );
+
+        float VdotH = dot( -rd, H );
+        vec3 F = F_Schlick( f0, VdotH );
+
+        colRem *= ( 1.0 - F ) * albedo / ( 1.0 - Fn );
+        rd = wo;
+      }
+
+      if ( dot( colRem, colRem ) < EPSILON ) {
+        break;
+      }
     }
-    col += isect.mtl.emissive * colRem;
 
-    ro += rd * isect.l + isect.N * EPSILON;
-
-    vec3 albedo = mix( 0.96 * isect.mtl.albedo, vec3( 0.0 ), isect.mtl.metallic );
-    vec3 f0 = mix( vec3( 0.04 ), isect.mtl.albedo, isect.mtl.metallic );
-
-    if ( random() < 0.5 ) {
-      // specular
-      // Ref: http://gregory-igehy.hatenadiary.com/entry/2015/02/26/154142
-      vec3 H = importanceSampleGGX( isect.mtl.roughness, isect.N );
-      vec3 wo = reflect( rd, H );
-      if ( dot( wo, isect.N ) < 0.0 ) { break; }
-
-      float VdotH = dot( -rd, H );
-      float NdotL = dot( isect.N, wo );
-      float NdotH = dot( isect.N, H );
-      float NdotV = dot( isect.N, -rd );
-      vec3 F = F_Schlick( f0, VdotH );
-      float G = G_Smith( isect.mtl.roughness, NdotV, NdotL );
-
-      colRem *= F * G * VdotH / ( NdotH * NdotV );
-      rd = wo;
-    } else {
-      // diffuse
-      vec3 wo = importanceSampleLambert( isect.N );
-      vec3 H = normalize( -rd + wo );
-
-      float VdotH = dot( -rd, H );
-      vec3 F = F_Schlick( f0, VdotH );
-
-      colRem *= ( 1.0 - F ) * albedo;
-      rd = wo;
-    }
-
-    colRem *= 2.0;
-
-    if ( dot( colRem, colRem ) < EPSILON ) {
-      break;
-    }
+    fragColor += vec4( col, 1.0 );
   }
-
-  fragColor += vec4( col, 1.0 );
 }
 `,SE=`#version 300 es
 
@@ -1000,7 +1036,7 @@ void main() {
 
   fragColor = vec4( color, 1 );
 }
-`,kE="./assets/reference.593f5bdb.png";const CE={width:512,height:256,screenLayer:"layerPresent",layers:[{name:"layerDraw",code:xE,textures:[{name:"samplerRandom",url:V1,filter:"linear"}]},{name:"layerAccumulate",code:wE},{name:"layerPresent",code:SE,textures:[{name:"samplerReference",url:kE,filter:"nearest"}]}]};var bE=`#define MARCH_ITER 128
+`,kE="./assets/reference.0066d56e.png";const CE={width:512,height:256,screenLayer:"layerPresent",layers:[{name:"layerDraw",code:xE,textures:[{name:"samplerRandom",url:V1,filter:"linear"}]},{name:"layerAccumulate",code:wE},{name:"layerPresent",code:SE,textures:[{name:"samplerReference",url:kE,filter:"nearest"}]}]};var bE=`#define MARCH_ITER 128
 #define MARCH_EPSILON 1E-2
 #define MARCH_NEAR_ENOUGH 1E-2
 #define MARCH_WAY_NEAR 1E-3
